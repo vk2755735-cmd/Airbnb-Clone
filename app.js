e6846aa5edcfe5a70302592fe74fec802224975f
Delta Student <vishalsanmth@gmail.com>
2026-02-12 14:59:31 +0530

    Fix port for render deploy

--- app.js
+++ app.js
@@ -113,6 +113,9 @@ app.use((err, req, res, next) => {
    // res.status(statusCode).send(message);
 });
 
-app.listen(8080, () =>{
-    console.log("Server is listening to port 8080");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () =>{
+    console.log(`Server is listening to port ${PORT}`);
 });
+
